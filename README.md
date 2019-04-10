ebd317989567d8b08c35ec3ee22f9388130703cc
ccwq <scwqd@qq.com>
2019-04-10 19:08:31 +0800

    功能进一步完善,详情看README.md

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
-一个gulp的基本模板。包含less和pug的编译
+一个gulp的基本模板。包含less和pug的编译,并使用Live-server实现自动刷新的效果
 
 克隆后，先执行``yarn``或者``npm i``
 
-之后执行
+构建
 
+---
+
+```bash
+npm run build 
 ```
-npm run build-less-pug 
+<br/>
+
+---
+
+调试
+```bash
+npm run dev
 ```
 
-将开始跟踪 *.less和*.pug文件的变化并编译
+调试模式下开始跟踪文件的变化自动编译并且刷新
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
 // 加载gulp
 var gulp = require('gulp');
+
 //加载gulp-load-plugins插件，并马上运行它
 var plugins = require('gulp-load-plugins')();
+
 var rename = require('gulp-rename');
 
-gulp.task('pug', async function () {
-    gulp.src('www/**/*.pug') // 传入管道的文件
+const argv = require('yargs').argv;
+
+const package = require("./package.json");
+
+const SRC_DIR = package["path for develop"];
+const DIST_DIR = package["path for product"];
+
+const rm = require("rimraf");
+
+gulp.task(':pug', async function () {
+    await gulp.src(`${SRC_DIR}**/*.pug`) // 传入管道的文件
         .pipe(plugins.plumber())
         .pipe(plugins.pug({
             pretty: true // 默认为false，表示是否美化HTML
@@ -13,23 +24,63 @@ gulp.task('pug', async function () {
         .pipe(rename(function(path){
             path.extname=".html"
         }))
-        .pipe(gulp.dest('www'))
+        .pipe(gulp.dest(SRC_DIR))
     ; // dest:destination
 });
 
 var less = require('gulp-less');
-gulp.task("less", async function(){
-    gulp.src("www/**/*.less")
+gulp.task(":less", async function(){
+    await gulp.src(`${SRC_DIR}**/*.less`)
         .pipe(less({
 
         }))
-        .pipe(gulp.dest("www"))
+        .pipe(gulp.dest(SRC_DIR))
     ;
 });
 
 
+gulp.task("watch-less-pug", async function () {
+    gulp.watch(`${SRC_DIR}**/*.pug`, gulp.series(":pug"));
+    gulp.watch(`${SRC_DIR}**/*.less`, gulp.series(":less"));
+});
 
-gulp.task("watch-less-pug", async function(){
-    gulp.watch("www/**/*.pug", gulp.series("pug"))
-    gulp.watch("www/**/*.less", gulp.series("less"))
+
+gulp.task(":copy", async function () {
+    gulp.src([
+        `${SRC_DIR}**/!(*.less|*.pug)`,
+    ])
+        .pipe(gulp.dest(`${DIST_DIR}`))
+    ;
+});
+
+gulp.task(":live-server", async function(){
+    const liveServer = require("live-server");
+    liveServer.start({
+        port: argv.port || 6524, // Set the server port. Defaults to 8080.
+        host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+        root: SRC_DIR, // Set root directory that's being served. Defaults to cwd.
+        open: true, // When false, it won't load your browser by default.
+        ignore: 'less,pug,my/templates', // comma-separated string for paths to ignore
+        wait: 300, // Waits for all changes, before reloading. Defaults to 0 sec.
+        logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+        middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
+    });
 })
+
+gulp.task(":cleanDist", async function(){
+    return new Promise(resolve=>{
+        rm(DIST_DIR, resolve)
+    })
+});
+
+
+/**
+ * 编译文件到设置的目录
+ */
+gulp.task("build", gulp.series(":cleanDist", ":pug", ":less", ":copy"));
+
+
+/**
+ * 调试模式
+ */
+gulp.task("dev", gulp.parallel("watch-less-pug",":live-server"));

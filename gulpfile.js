0ae0542a616b7363343c1d2190a822290c6e99a0
ccwq <scwqd@qq.com>
2019-04-11 09:44:25 +0800

    编辑less文件时，页面自动刷新的问题解决

--- gulpfile.js
+++ gulpfile.js
@@ -60,7 +60,7 @@ gulp.task(":live-server", async function(){
         host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
         root: SRC_DIR, // Set root directory that's being served. Defaults to cwd.
         open: true, // When false, it won't load your browser by default.
-        ignore: 'less,pug,my/templates', // comma-separated string for paths to ignore
+        ignore: '**/*.@(less|pug)', // comma-separated string for paths to ignore
         wait: 300, // Waits for all changes, before reloading. Defaults to 0 sec.
         logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
         middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
